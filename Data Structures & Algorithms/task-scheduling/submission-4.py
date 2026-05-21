6021ab823222ad9c046f1f85b903a81089d3c735
AeschyJ <45564481+AeschyJ@users.noreply.github.com>
2026-05-22 01:42:50 +0800

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        from collections import Counter
+        l = [[a, i, 0] for a, i in list(Counter(tasks).items())]
+        t = 0
+        while l:
+            l.sort(key = lambda x: (1 if x[2] <= t else 0, x[1]), reverse = True)
+            print(l, t)
+            if l[0][1] == 0:
+                break
+            if l[0][2] > t:
+                ...
+            else:
+                l[0][1] -= 1
+                if l[0][1] == 0:
+                    l[0][2] = math.inf
+                else: l[0][2] = t + n + 1
+            t += 1
+        return t
