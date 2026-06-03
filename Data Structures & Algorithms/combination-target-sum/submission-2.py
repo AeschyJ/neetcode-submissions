3c5c0a4379348b43e55449677d348b41cd532c6d
AeschyJ <45564481+AeschyJ@users.noreply.github.com>
2026-06-04 01:30:16 +0800

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        a = []
+        nums.sort()
+        def travel(arr, nums, target, curridx):
+            for idx, n in enumerate(nums):
+                if idx < curridx:
+                    continue
+                elif target < n:
+                    continue
+                elif target == n:
+                    arr.append(n)
+                    a.append(arr.copy())
+                    arr.pop()
+                else:
+                    arr.append(n)
+                    travel(arr, nums, target - n, idx)
+                    arr.pop()
+        travel([], nums, target, 0)
+        return a
