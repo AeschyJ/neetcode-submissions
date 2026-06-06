f2d21cf1cd0171595a21878c2998e613dc68535a
AeschyJ <45564481+AeschyJ@users.noreply.github.com>
2026-06-06 21:24:55 +0800

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = set()
+        def travel(n):
+            # if n == 1:
+            #     return ["()"]
+            # else:
+            r = []
+            for i in range(n-1):
+                x = travel(i+1)
+                y = travel(n-i-1)
+                for a in x:
+                    for b in y:
+                        r.append(a+b)
+            if n > 1:
+                for a in travel(n-1):
+                    r.append("(" + a + ")")
+            else:
+                r.append("()")
+            return r
+        for i in travel(n):
+            ans.add(i)
+        return list(ans)
