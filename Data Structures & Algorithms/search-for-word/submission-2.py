8e05b7f4b7b9d843458a2aa7485ad629c330815e
AeschyJ <45564481+AeschyJ@users.noreply.github.com>
2026-06-07 02:27:43 +0800

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.a = False
+        path = []
+        def travel(x, y, idx):
+            if self.a:
+                return
+            if idx >= len(word):
+                self.a = True
+                return
+            path.append((x, y))
+            if x > 0:
+                if (x - 1, y) in path:
+                    pass
+                elif board[y][x - 1] == word[idx]:
+                    travel(x - 1, y, idx + 1)
+            if x < len(board[0]) - 1:
+                if (x + 1, y) in path:
+                    pass
+                elif board[y][x + 1] == word[idx]:
+                    travel(x + 1, y, idx + 1)
+            if y > 0:
+                if (x, y - 1) in path:
+                    pass
+                elif board[y - 1][x] == word[idx]:
+                    travel(x, y - 1, idx + 1)
+            if y < len(board) - 1:
+                if (x, y + 1) in path:
+                    pass
+                elif board[y + 1][x] == word[idx]:
+                    travel(x, y + 1, idx + 1)
+            path.pop()
+        for idx, row in enumerate(board):
+            if self.a:
+                break
+            if word[0] in row:
+                ids = [i for i, c in enumerate(row) if c == word[0]]
+                for i in ids:
+                    travel(i, idx, 1)
+        return self.a
