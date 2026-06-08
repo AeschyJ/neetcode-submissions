2ff8e9258b4ea3b9a80cfe69ae1be34a18cc06ba
AeschyJ <45564481+AeschyJ@users.noreply.github.com>
2026-06-09 00:11:03 +0800

    Add: combinations-of-a-phone-number - submission-4

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        from itertools import product
+        if not digits:
+            return []
+        
+        phone = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        
+        return ["".join(comb) for comb in product(*(phone[d] for d in digits))]
