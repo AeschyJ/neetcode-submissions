0a91de3cfe1e0fabcd48ddd467c40d657b606bf1
AeschyJ <45564481+AeschyJ@users.noreply.github.com>
2026-06-09 20:23:12 +0800

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,34 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.d = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.d
+        for c in word:
+            if c in curr:
+                curr = curr[c]
+            else:
+                curr[c] = {}
+                curr = curr[c]
+        curr['.'] = True
+
+    def search(self, word: str) -> bool:
+        curr = self.d
+        for c in word:
+            if c in curr:
+                curr = curr[c]
+            else:
+                return False
+        if '.' in curr: return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.d
+        for c in prefix:
+            if c in curr:
+                curr = curr[c]
+            else:
+                return False
+        return True
+        
